dcd8a9bb9b6a7df27a3deae23edc473d2d28db11
Ramki Balakrishnan <ramkicsebe@gmail.com>
2025-07-15 12:27:52 +0530

    Added HTTP

--- hello.js
+++ hello.js
@@ -1,1 +1,10 @@
-console.log("Hello, World!");
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello, World from Load Balancer!\n');
+});
+
+server.listen(80, () => {
+  console.log('Server running on port 80');
+});
